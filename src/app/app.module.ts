fbadf1a3a8d41b0e8636e9ffd672110252e3769d
Dailan Bueno <daylansantos@gmail.com>
2018-02-17 16:55:40 -0200

    Criando seletor receita e despesa.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {DataTableModule} from 'primeng/components/datatable/datatable';
 import {TooltipModule} from 'primeng/components/tooltip/tooltip';
 import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
 import {CalendarModule} from 'primeng/calendar';
+import {SelectButtonModule} from 'primeng/selectbutton';
 
 import { LoncamentoPesquisaComponent } from './loncamento-pesquisa/loncamento-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -31,7 +32,8 @@ import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-ca
     DataTableModule,
     TooltipModule,
     InputTextareaModule,
-    CalendarModule
+    CalendarModule,
+    SelectButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
